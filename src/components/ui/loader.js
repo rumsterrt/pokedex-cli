e44e0f9f5c788f5b9db9b95e1f14823210ed736e
Anatoly Ryumkin <rumsterrt@gmail.com>
2020-02-11 15:21:45 +0900

    Fixes

--- src/components/ui/loader.js
+++ src/components/ui/loader.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react'
+import Backdrop from '@material-ui/core/Backdrop'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-  },
-}));
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+}))
 
 export default function SimpleBackdrop() {
-  const classes = useStyles();
+    const classes = useStyles()
 
-  return (
-      <Backdrop className={classes.backdrop} open={true}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
-  );
+    return (
+        <Backdrop className={classes.backdrop} open={true}>
+            <CircularProgress color="inherit" />
+        </Backdrop>
+    )
 }
